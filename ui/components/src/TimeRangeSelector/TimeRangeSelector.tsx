c644a71163398207a203925826a9686da3b2f9c2
Steven Cobb <sjcobb.dev@gmail.com>
2022-07-22 12:31:41 -0400

    add static end time using DateTimePicker
    Signed-off-by: Steven Cobb <sjcobb.dev@gmail.com>

--- ui/components/src/TimeRangeSelector/TimeRangeSelector.tsx
+++ ui/components/src/TimeRangeSelector/TimeRangeSelector.tsx
@@ -13,7 +13,7 @@
 
 import { useState } from 'react';
 import { TextField } from '@mui/material';
-import { DatePicker, DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 // import { isValid, format } from 'date-fns';
 import { format } from 'date-fns';
@@ -22,26 +22,24 @@ const DATE_TIME_FORMAT = 'E MMM dd yyyy HH:mm:ss OOOO';
 
 interface TimeRangeSelectorProps {
   start: number;
+  end: number;
 }
 
-export const TimeRangeSelector = ({ start }: TimeRangeSelectorProps) => {
+export const TimeRangeSelector = ({ start, end }: TimeRangeSelectorProps) => {
   const [startDate, setStartDate] = useState<string>(format(start, DATE_TIME_FORMAT));
-
-  // const [endDate, setEndDate] = useState<string>(
-  //   format(value.end, DATE_TIME_FORMAT)
-  // );
+  const [endDate, setEndDate] = useState<string>(format(end, DATE_TIME_FORMAT));
 
   const handleSelectStartDate = (start: string) => {
     setStartDate(start);
   };
 
-  // const handleSelectEndDate = (end: string) => {
-  //   setEndDate(end);
-  // };
+  const handleSelectEndDate = (end: string) => {
+    setEndDate(end);
+  };
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DatePicker
+      {/* <DatePicker
         renderInput={(props) => <TextField {...props} />}
         label="Start Date"
         value={startDate}
@@ -49,14 +47,23 @@ export const TimeRangeSelector = ({ start }: TimeRangeSelectorProps) => {
           const newDate = newValue ?? '1658153703847';
           handleSelectStartDate(newDate);
         }}
+      /> */}
+      <DateTimePicker
+        renderInput={(props) => <TextField {...props} />}
+        label="Start Time"
+        value={startDate}
+        onChange={(newValue) => {
+          if (newValue === null) return;
+          handleSelectStartDate(newValue);
+        }}
       />
       <DateTimePicker
         renderInput={(props) => <TextField {...props} />}
-        label="Start Date / Time"
-        value={startDate}
+        label="End Time"
+        value={endDate}
         onChange={(newValue) => {
-          const newDate = newValue ?? '1658153703847';
-          handleSelectStartDate(newDate);
+          if (newValue === null) return;
+          handleSelectEndDate(newValue);
         }}
       />
     </LocalizationProvider>
--- ui/dashboards/src/views/ViewDashboard.tsx
+++ ui/dashboards/src/views/ViewDashboard.tsx
@@ -62,7 +62,7 @@ export function ViewDashboard(props: ViewDashboardProps) {
                 {dashboardResource.metadata.name}
               </Typography>
               <Stack direction="row" spacing={1}>
-                <TimeRangeSelector start={new Date().getTime()} />
+                <TimeRangeSelector start={new Date().getTime()} end={new Date().getTime()} />
               </Stack>
             </PageHeader>
             <Dashboard spec={dashboardResource.spec} />
