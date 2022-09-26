571beb0ee646314476b36d137cda9abf071001a4
Augustin Husson <husson.augustin@gmail.com>
2022-09-26 11:24:53 +0200

    check npm config cache is actually set
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+echo "${HOME}"
+
 if ! [[ -w  $HOME ]]
 then
+  echo "export npm cache config"
   export npm_config_cache=$(mktemp -d)
 fi
+
+echo "${npm_config_cache}"
 
 cd ui/
 npm install
